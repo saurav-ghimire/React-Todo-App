def99d113cad3d76ee1297e85bbc1d2b504431e8
saurav-ghimire <sauravghimire0123@gmail.com>
2024-02-15 15:30:25 -0500

    Bootstrap Installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
 
   return (
     <>
-      
+      <div className="container">
+        <h2>My React Todo</h2>
+      </div>
     </>
   )
 }
